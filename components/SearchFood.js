69674a3d8dcf67e39e1c6e1284f5d94f599f1205
Paulo Alves <paulovalves@outlook.com>
2021-02-12 05:34:18 -0300

    Search by the name

--- components/SearchFood.js
+++ components/SearchFood.js
@@ -1,47 +1,57 @@
-import React, {useState} from 'react'
-import { View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native'
-import Icon from 'react-native-vector-icons/dist/FontAwesome'
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
-const AddFood =({ searchItem }) => {
-
+const AddFood = ({searchItem}) => {
   const [searchString, setSearchString] = useState('');
   const onChange = (textValue) => {
-    setSearchString(textValue)
-  }
+    setSearchString(textValue);
+  };
 
   const search = (searchString) => {
-    searchItem(searchString)
-  }
+    searchItem(searchString);
+  };
 
-  return(
+  return (
     <View>
-      <TextInput placeholder='Selecione o alimento' style={styles.input} onChangeText={onChange}/>
+      <TextInput
+        placeholder="Selecione o alimento"
+        style={styles.input}
+        onChangeText={onChange}
+      />
       <TouchableOpacity style={styles.btn} onPress={() => search(searchString)}>
         <Text style={styles.btnText}>
-          <Icon name='search' size={20}/> Procurar</Text>
+          <Icon name="search" size={20} /> Procurar
+        </Text>
       </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   input: {
     height: 60,
     padding: 8,
-    fontSize: 16
+    fontSize: 16,
   },
 
   btn: {
     backgroundColor: '#c2bad8',
     padding: 9,
-    margin: 5
+    margin: 5,
   },
 
   btnText: {
     color: '#3311cc',
     fontSize: 20,
-    textAlign: 'center'
-  }
-})
+    textAlign: 'center',
+  },
+});
 
 export default AddFood;
--- components/SearchResult.js
+++ components/SearchResult.js
@@ -1,16 +1,142 @@
-import React from 'react'
-import { FlatList, View} from 'react-native'
-import ResultItem from './ResultItem'
+import React, {useState} from 'react';
+import {
+  FlatList,
+  View,
+  Modal,
+  Alert,
+  Pressable,
+  Text,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+import ResultItem from './ResultItem';
 
 const SearchResult = ({searchList, addToRecipe}) => {
-  return(
+  const [modalVisible, setModalVisible] = useState(false);
+  const [id, setId] = useState('');
+  const [quantity, setQuantity] = useState(0);
+
+  const openModal = (id) => {
+    setModalVisible(true);
+    setId(id);
+  };
+
+  const getData = (id, quantity) => {
+    if (!quantity) {
+      Alert.alert('Erro', 'Informe a quantidade');
+    } else {
+      addToRecipe(id, quantity);
+      setQuantity(0);
+      setModalVisible(!modalVisible);
+      Alert.alert('Adicionado à receita');
+    }
+  };
+
+  return (
     <View>
-      <FlatList data={searchList} renderItem={({item}) => (
-        <ResultItem item={item} addToRecipe={addToRecipe}/>
-        )} keyExtractor={item => item.id}
+      <FlatList
+        data={searchList}
+        renderItem={({item}) => (
+          <ResultItem
+            item={item}
+            addToRecipe={addToRecipe}
+            openModal={openModal}
+          />
+        )}
+        keyExtractor={(item) => item.id}
       />
+      <View style={styles.centerView}>
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={modalVisible}
+          onREquestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
+          }}>
+          <View style={styles.centerView}>
+            <View style={styles.modalView}>
+              <Pressable style={styles.close}>
+                <Icon
+                  name="times"
+                  style={styles.closeFont}
+                  onPress={() => {
+                    setModalVisible(!modalVisible);
+                  }}
+                />
+              </Pressable>
+              <TextInput
+                style={styles.textInput}
+                keyboardType="number-pad"
+                placeholder="Adicione a quantidade"
+                value={quantity}
+                onChangeText={(text) => setQuantity(text)}
+              />
+              <Pressable style={styles.button} onPress={getData(id, quantity)}>
+                <Text style={styles.textStyle}>Adicionar</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+      </View>
     </View>
-      )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  centerView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+    marginTop: 22,
+  },
+  textInput: {
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 20,
+    textAlign: 'center',
+    height: 60,
+    padding: 8,
+    fontSize: 16,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 15,
+    elevation: 2,
+    marginTop: 10,
+    backgroundColor: '#0161e8',
+  },
+  textStyle: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  close: {
+    marginRight: -300,
+    marginTop: -10,
+  },
+  closeFont: {
+    fontSize: 30,
+    color: '#cc2211',
+  },
+});
 
 export default SearchResult;
