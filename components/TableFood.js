531ccb758a57b90a3c5be611a09985dac61932ce
Paulo Alves <paulovalves@outlook.com>
2021-02-14 06:30:04 -0300

    Design the Table to show the nutritional values

---
+++ components/TableFood.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+const TableFood = ({myList}) => {
+  let item;
+  return(
+    <View style={styles.view}>
+      <View style={styles.foodItem}>
+          <Text style={styles.text}>Valor Energético </Text>
+          <Text style={styles.text}>{item}kcal = {item}kJ</Text>        
+      </View>
+      <View style={styles.foodItem}>
+        <Text style={styles.text}>Carboidratos</Text>
+        <Text style={styles.text}> {item}g</Text>
+      </View>      
+      <View style={styles.foodItem}>
+        <Text style={styles.text}>Proteínas</Text>
+        <Text style={styles.text}> {item}g</Text>
+      </View>
+
+      <View style={styles.foodItem}>
+        <Text style={styles.text}>Gorduras totais</Text>
+        <Text style={styles.text}> {item}g</Text>
+      </View>
+
+      <View style={styles.foodItem}>
+        <Text style={styles.text}>Gorduras saturadas</Text>
+        <Text style={styles.text}> {item}g</Text>
+      </View>
+      
+      <View style={styles.foodItem}>
+        <Text style={styles.text}>Gorduras trans</Text>
+        <Text style={styles.text}>{item}g</Text>
+      </View>
+  
+      <View style={styles.foodItem}>
+        <Text style={styles.text}>Colesterol</Text>
+        <Text style={styles.text}>{item}mg</Text>
+      </View>
+  
+      <View style={styles.foodItem}>
+        <Text style={styles.text}>Fibra alimentar</Text>
+        <Text style={styles.text}>{item}g</Text>
+      </View>
+
+      <View style={styles.foodItem}>
+        <Text style={styles.text}>Sódio</Text>
+        <Text style={styles.text}>{item}mg</Text>
+      </View>
+  
+      <View style={styles.foodItem}>
+        <Text style={styles.text}>Ferro</Text>
+        <Text style={styles.text}>{item}mg</Text>
+      </View>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  view:{
+    flex: 1,
+  },
+
+  text: {
+    fontSize: 22,
+
+  },
+  
+  foodItem:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding:5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
+  }
+})
+
+export default TableFood;
