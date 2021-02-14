717d0a59a63d294faa272239e694fc4326441df3
Paulo Alves <paulovalves@outlook.com>
2021-02-14 07:11:33 -0300

    Better styling the TableFood

--- components/TableFood.js
+++ components/TableFood.js
@@ -5,6 +5,9 @@ const TableFood = ({myList}) => {
   let item;
   return(
     <View style={styles.view}>
+      <View style={styles.viewHeader}>
+        <Text style={styles.textHeader}>Porção de {item}g</Text>
+      </View>
       <View style={styles.foodItem}>
           <Text style={styles.text}>Valor Energético </Text>
           <Text style={styles.text}>{item}kcal = {item}kJ</Text>        
@@ -60,6 +63,19 @@ const TableFood = ({myList}) => {
 const styles = StyleSheet.create({
   view:{
     flex: 1,
+    marginTop: 40
+  },
+
+  viewHeader:{
+    justifyContent: 'center',
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
+  },
+
+  textHeader:{
+    textAlign: 'center',
+    fontSize: 22
   },
 
   text: {
